18d46f4beb6e0916cbdf70ecb513375d594188a7
Miguel Celis <60907205+mcelis025@users.noreply.github.com>
2020-09-17 21:42:36 -0700

    changed footer color and focus color

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -1,36 +1,42 @@
 import React from "react";
+import "./style.css";
 
 function Contact() {
   return (
     <div className="container">  
       <h1 className="center">contact.</h1>
-    <div className="row">
-      <form className="col s12">
-        <div className="row">
-          <div className="input-field col s6">
-            <input id="name" type="text" className="validate"></input>
-            <label for="name">Name</label>
+      <div className="row">
+        <form className="col s12">
+          <div className="row">
+            <div className="input-field col s6">
+              <input id="name" type="text" className="validate"></input>
+              <label for="name">Name.</label>
+            </div>
+            <div className="input-field col s6">
+              <input id="contactPhone" type="tel" className="validate"></input>
+              <label for="contactPhone">Phone.</label>
+            </div>
           </div>
-          <div className="input-field col s6">
-            <input id="contactPhone" type="tel" className="validate"></input>
-            <label for="contactPhone">Phone</label>
+          <div className="row">
+            <div className="input-field col s12">
+              <input id="contactEmail" type="email" className="validate"></input>
+              <label for="contactEmail">Email.</label>
+              <span className="helper-text" data-error="Check Format"></span>
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="input-field col s12">
-            <input id="contactEmail" type="email" className="validate"></input>
-            <label for="contactEmail">Email</label>
-            <span className="helper-text" data-error="Check Format"></span>
+          <div className="row">
+            <div class="input-field col s12">
+              <textarea id="contactMessage" className="materialize-textarea validate"></textarea>
+              <label for="contactMessage">Enter Your Message.</label>
+            </div>
           </div>
-        </div>
-        <div className="row">
-        <div class="input-field col s12">
-          <textarea id="contactMessage" className="materialize-textarea"></textarea>
-          <label for="contactMessage">Enter Your Message.</label>
-        </div>
-        </div>
-      </form>
-    </div>
+          <button class="btn btn-small right waves-effect waves-light black" type="submit" name="action">Send
+            <i class="material-icons right">send</i>
+          </button>
+        </form>
+      </div>
+      <br />
+      <br />
     </div>
   );
 }
--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="page-footer">
+    <footer className="page-footer blue darken-4">
       <div className="footer-copyright">
         <div className="container center">
           © 2020 Created by Miguel Celis
